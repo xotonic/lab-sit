2da7fbc9e8f78e0b405dd1a85639e554bd847bb1
xotonic <xotonic@yandex.ru>
2016-11-28 02:53:04 +0700

    FactorySettings logic. Need add tooltips, labes, statistics dialog

--- src/main/java/com/xotonic/lab/sit/settings/SettingsModel.java
+++ src/main/java/com/xotonic/lab/sit/settings/SettingsModel.java
@@ -1,24 +1,13 @@
 package com.xotonic.lab.sit.settings;
 
 
-import java.io.Serializable;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 
-public class SettingsModel implements Serializable {
-    public SimulationState simulationState;
+public class SettingsModel implements Model {
+
+    SimulationState simulationState;
     boolean showInfo;
     boolean showTime;
-    Map<FactoryType, FactorySettings> factoriesSettings = new HashMap<>();
-
-    {
-        Arrays.stream(FactoryType.values())
-                .forEach(type -> factoriesSettings.put(type, new FactorySettings()));
-    }
 
     enum SimulationState {start, stop, pause}
-
-    enum FactoryType {car, bike;}
 }
